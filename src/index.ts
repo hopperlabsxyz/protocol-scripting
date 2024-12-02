bf7abcca2500fe2b34cde59bd15e42b752fb5ed8
Julien FRANCAIS <jfrancai@student.42.fr>
2024-12-02 20:16:25 +0100

    refactoring code

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,11 @@
-import { createPublicClient, getContract, http } from 'viem';
-import { arbitrum } from 'viem/chains';
-import fs from 'fs';
+import aave from "./aave";
+import { arb1Client, dumpOjectToFile } from "./utils";
 
-export const publicClient = createPublicClient({
-    chain: arbitrum,
-    transport: http(),
+(async function main() {
+  dumpOjectToFile(await aave.getProtocolData(arb1Client), "arb1");
+})()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
   });
-
-// PROTOCOL_DATA_PROVIDER
-const PROTOCOL_DATA_PROVIDER = "0x7F23D86Ee20D869112572136221e173428DD740B";
-
-//ABIs //
-const ABIs = [{"inputs":[{"internalType":"contract IPoolAddressesProvider","name":"addressesProvider","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ADDRESSES_PROVIDER","outputs":[{"internalType":"contract IPoolAddressesProvider","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getATokenTotalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getAllATokens","outputs":[{"components":[{"internalType":"string","name":"symbol","type":"string"},{"internalType":"address","name":"tokenAddress","type":"address"}],"internalType":"struct AaveProtocolDataProvider.TokenData[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getAllReservesTokens","outputs":[{"components":[{"internalType":"string","name":"symbol","type":"string"},{"internalType":"address","name":"tokenAddress","type":"address"}],"internalType":"struct AaveProtocolDataProvider.TokenData[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getDebtCeiling","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getDebtCeilingDecimals","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getInterestRateStrategyAddress","outputs":[{"internalType":"address","name":"irStrategyAddress","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getLiquidationProtocolFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getPaused","outputs":[{"internalType":"bool","name":"isPaused","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getReserveCaps","outputs":[{"internalType":"uint256","name":"borrowCap","type":"uint256"},{"internalType":"uint256","name":"supplyCap","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getReserveConfigurationData","outputs":[{"internalType":"uint256","name":"decimals","type":"uint256"},{"internalType":"uint256","name":"ltv","type":"uint256"},{"internalType":"uint256","name":"liquidationThreshold","type":"uint256"},{"internalType":"uint256","name":"liquidationBonus","type":"uint256"},{"internalType":"uint256","name":"reserveFactor","type":"uint256"},{"internalType":"bool","name":"usageAsCollateralEnabled","type":"bool"},{"internalType":"bool","name":"borrowingEnabled","type":"bool"},{"internalType":"bool","name":"stableBorrowRateEnabled","type":"bool"},{"internalType":"bool","name":"isActive","type":"bool"},{"internalType":"bool","name":"isFrozen","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getReserveData","outputs":[{"internalType":"uint256","name":"unbacked","type":"uint256"},{"internalType":"uint256","name":"accruedToTreasuryScaled","type":"uint256"},{"internalType":"uint256","name":"totalAToken","type":"uint256"},{"internalType":"uint256","name":"totalStableDebt","type":"uint256"},{"internalType":"uint256","name":"totalVariableDebt","type":"uint256"},{"internalType":"uint256","name":"liquidityRate","type":"uint256"},{"internalType":"uint256","name":"variableBorrowRate","type":"uint256"},{"internalType":"uint256","name":"stableBorrowRate","type":"uint256"},{"internalType":"uint256","name":"averageStableBorrowRate","type":"uint256"},{"internalType":"uint256","name":"liquidityIndex","type":"uint256"},{"internalType":"uint256","name":"variableBorrowIndex","type":"uint256"},{"internalType":"uint40","name":"lastUpdateTimestamp","type":"uint40"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getReserveEModeCategory","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getReserveTokensAddresses","outputs":[{"internalType":"address","name":"aTokenAddress","type":"address"},{"internalType":"address","name":"stableDebtTokenAddress","type":"address"},{"internalType":"address","name":"variableDebtTokenAddress","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getSiloedBorrowing","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getTotalDebt","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getUnbackedMintCap","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"},{"internalType":"address","name":"user","type":"address"}],"name":"getUserReserveData","outputs":[{"internalType":"uint256","name":"currentATokenBalance","type":"uint256"},{"internalType":"uint256","name":"currentStableDebt","type":"uint256"},{"internalType":"uint256","name":"currentVariableDebt","type":"uint256"},{"internalType":"uint256","name":"principalStableDebt","type":"uint256"},{"internalType":"uint256","name":"scaledVariableDebt","type":"uint256"},{"internalType":"uint256","name":"stableBorrowRate","type":"uint256"},{"internalType":"uint256","name":"liquidityRate","type":"uint256"},{"internalType":"uint40","name":"stableRateLastUpdated","type":"uint40"},{"internalType":"bool","name":"usageAsCollateralEnabled","type":"bool"}],"stateMutability":"view","type":"function"}] as const;
-
-async function main(){
-
-  // 1. Create contract instance
-  const contract = getContract({
-      address: PROTOCOL_DATA_PROVIDER,
-      abi: ABIs,
-      client: publicClient,
-    })
-
-    // 2. call getAllReservesTokens 
-  const reservesTokens = await contract.read.getAllReservesTokens();
-
-  const result = await Promise.all(reservesTokens.map(async (reserveToken) => {
-    // 3. for each reserve token, get the Config Data 
-    const configDataRaw = await contract.read.getReserveConfigurationData([reserveToken.tokenAddress]);
-
-    // 4. get the Tokens Addresses
-    const tokenAddressRaw = await contract.read.getReserveTokensAddresses([reserveToken.tokenAddress]);
-  
-    const reserveData = {
-      symbol: reserveToken.symbol,
-      token: reserveToken.tokenAddress,
-
-      usageAsCollateralEnabled: configDataRaw[5],
-      borrowingEnabled: configDataRaw[6],
-      stableBorrowRateEnabled: configDataRaw[7],
-      isActive: configDataRaw[8],
-      isFrozen: configDataRaw[9],
-
-      aTokenAddress: tokenAddressRaw[0],
-      // stableDebtTokenAddress: tokenAddressRaw[1],
-      variableDebtTokenAddress: tokenAddressRaw[2],
-    };
-
-    return reserveData;
-  }
-  ));
-
-  // Convert the result to a JSON string with indentation
-  const jsonString = JSON.stringify(result, null, 4);
-
-  // Remove quotes around keys using a regex
-  const formatedString = jsonString.replace(/"([^"]+)":/g, '$1:');
-
-  // Add the prefix and suffix
-  const res = `// This file is auto-generated. Do not edit!\n\nexport default ${formatedString} as const`;
-
-  const protocolName = "arb1"; //define the protocol name
-
-  // Specify the file name
-  const fileName = "out/_" + protocolName + "Info.ts";
-
-  // Write to the file
-  fs.writeFileSync(fileName, res);
-}
-
-main().then(() => process.exit(0)).catch(error => {
-  console.error(error);
-  process.exit(1);
-});
---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+import { createPublicClient, http } from "viem";
+import { arbitrum } from "viem/chains";
+import fs from "fs";
+
+export const arb1Client = createPublicClient({
+  chain: arbitrum,
+  transport: http(),
+});
+
+export function dumpOjectToFile(o: Object, fileName: string) {
+  // Convert the result to a JSON string with indentation
+  const jsonString = JSON.stringify(o, null, 4);
+
+  // Remove quotes around keys using a regex
+  const formatedString = jsonString.replace(/"([^"]+)":/g, "$1:");
+
+  // Add the prefix and suffix
+  const res = `// This file is auto-generated. Do not edit!\n\nexport default ${formatedString} as const`;
+
+  // Write to the file
+  fs.writeFileSync("out/_" + fileName + "Info.ts", res);
+}
